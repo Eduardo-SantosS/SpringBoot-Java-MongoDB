fdfa2b34e565e89f187a06ae7509203667a9e93e
Eduardo-SantosS <santos.eduardopro@gmail.com>
2020-12-11 19:44:31 -0300

    Using DTO to return users

--- src/main/java/com/spring/mongo/Restful/resources/UserResource.java
+++ src/main/java/com/spring/mongo/Restful/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.spring.mongo.Restful.resources;
 
 import com.spring.mongo.Restful.domain.User;
+import com.spring.mongo.Restful.dto.UserDto;
 import com.spring.mongo.Restful.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -18,7 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        return ResponseEntity.ok().body(userService.findAll());
+    public ResponseEntity<List<UserDto>> findAll(){
+        List<User> list = userService.findAll();
+        List<UserDto> dtoList = list.stream().map(UserDto::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtoList);
     }
 }
